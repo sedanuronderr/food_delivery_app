673368b85b60e72cd86ed30325f8937f1d6bbebd
Seda Nur Önder <sednrondr@gmail.com>
2024-03-22 15:21:18 +0300

    Initial commit

---
+++ lib/app/food_delivery_app.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class FoodDeliveryApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'app/food_delivery_app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp( FoodDeliveryApp());
+}
+
